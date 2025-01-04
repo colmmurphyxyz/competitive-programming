3d203826c5797d2531d87d0592992ee40a5ccdb0
Colm Murphy <colmmurphy016@gmail.com>
2025-01-04 14:34:50 +0000

    leetcode: 2270

--- leetcode/2270_Number_of_Ways_to_Split_Array.java
+++ leetcode/2270_Number_of_Ways_to_Split_Array.java
@@ -2,10 +2,9 @@ class Solution {
     public int waysToSplitArray(int[] nums) {
         int n = nums.length;
         long[] sums = new long[n];
-        long[] revSums = new long[n];
         
         long sum = 0L;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // n iterations
             sum += nums[i];
             sums[i] = sum;
         }
