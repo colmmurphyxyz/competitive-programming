2f84f094df55dd61193d1386001465b8c8b34c13
Colm Murphy <colmmurphy016@gmail.com>
2025-03-11 22:07:52 +0000

    leetcode: 104

---
+++ leetcode-rs/src/_0104_maximum_depth_of_binary_tree.rs
@@ -0,0 +1,41 @@
+use std::cmp::max;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None
+        }
+    }
+}
+
+enum Solution {}
+
+impl Solution {
+    pub fn dfs(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                1 + max(
+                    Self::dfs(node.borrow().left.clone()),
+                    Self::dfs(node.borrow().right.clone())
+                )
+            }
+            None => 0
+        }
+    }
+
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::dfs(root)
+    }
+}
