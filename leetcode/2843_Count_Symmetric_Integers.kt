ab99071310ae3021bcd89bcd5bee6288c12eb86b
Colm Murphy <colmmurphy016@gmail.com>
2025-04-11 14:40:01 +0100

    leetcode: 2843

---
+++ leetcode/2843_Count_Symmetric_Integers.kt
@@ -0,0 +1,21 @@
+fun isSymmetric(x: Int): Boolean {
+    val s = x.toString()
+    val n = s.length
+    if (n % 2 == 1) return false
+    val lhs = s.take(n / 2)
+    val rhs = s.drop(n / 2)
+    val leftSum = lhs.foldRight(0) { digit, acc -> acc + digit.digitToInt() }
+    val rightSum = rhs.foldRight(0) { digit, acc -> acc + digit.digitToInt() }
+    // println("$x: leftSum=$leftSum, rightSum=$rightSum")
+    return leftSum == rightSum
+}
+
+fun countSymmetricIntegers(low: Int, high: Int): Int {
+    return (low .. high).filter(::isSymmetric).size
+}
+
+fun main() {
+    // println(countSymmetricIntegers(1, 25))
+    println(countSymmetricIntegers(1, 100))
+    println(countSymmetricIntegers(1200, 1230))
+}
